c52c24eabc9d03305a55a95c72ed25b2cd8df65b
Blaine Bublitz <phate666x@gmail.com>
2011-09-15 23:39:34 -0700

    Adapt.js files added, some might not be needed, need to test...

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
-/* Author:
+/* Author: 
 
 */
 
+var ADAPT_CONFIG = {
+  // Where is your CSS?
+  path: 'css/',
 
+  // false = Only run once, when page first loads.
+  // true = Change on window resize and page tilt.
+  dynamic: true,
 
-
-
+  // First range entry is the minimum.
+  // Last range entry is the maximum.
+  // Separate ranges by "to" keyword.
+  range: [
+    '0px    to 760px  = mobile.min.css',
+    '760px  to 980px  = 720.min.css',
+    '980px  to 1280px = 960.min.css',
+    '1280px to 1600px = 1200.min.css',
+    '1600px to 1920px = 1560.min.css',
+    '1940px to 2540px = 1920.min.css',
+    '2540px           = 2520.min.css'
+  ]
+};
