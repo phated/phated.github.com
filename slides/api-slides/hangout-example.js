675adf3d3de61896d36dcac625ccca5405c65fe2
Blaine Bublitz <phate666x@gmail.com>
2011-11-04 22:35:24 -0700

    separate code

---
+++ slides/api-slides/hangout-example.js
@@ -0,0 +1,34 @@
+/**
+ * @author phated
+ */
+(function startMyApp($) {
+	var tabs = new gadgets.TabSet(__MODULE_ID__); 
+
+	var participants = gapi.hangout.getParticipants();
+		$.each(participants, function(index, participant) {
+		  var tabId = tabs.addTab(participant.displayName);
+		  tabs.setSelectedTab(index);
+		  var gplusId = participant.id;
+		  $.get('https://www.googleapis.com/plus/v1/people/' + gplusId + '/activities/public?key=AIzaSyB14Ua7k5_wusxHTQEH3sqmglO7MHjHPCI&maxResults=5&pp=1&alt=json', function(data){
+			$.each(data.items, function(index, item) {
+			  if(item.provider.title === "Hangout") {
+			  	activityId = item.id;
+			  	$.get('https://www.googleapis.com/plus/v1/activities/' + activityId + '/comments?key=AIzaSyB14Ua7k5_wusxHTQEH3sqmglO7MHjHPCI&fields=items(actor%2Cobject%2Cpublished%2Cupdated)&alt=json', function(data){
+			      $.each(data.items, function(index, item) {
+			        var postedDate = new Date(item.published);
+			        var editedDate = new Date(item.updated);
+			        var output = '<img src="' + item.actor.image.url + '" width="100" height="100" style="float:left; margin:10px;" />';
+			        output += '<div style="float:left; margin:10px;">';
+			        output += '<a href="' + item.actor.url + '" target="_blank">' + item.actor.displayName + '</a>';
+			        output += ' - ' + item.object.content;
+			        output += '<div style="margin:15px 0;">Posted on: ' + postedDate.toLocaleString() + '</div>';
+			        output += '<div style="margin:15px 0;">Edited on: ' + editedDate.toLocaleString() + '</div>';
+			        output += '</div>';
+			        $('#' + tabId).append(output);
+			      });
+			    }, "jsonp");
+			  }
+			});
+	      }, "jsonp");
+		})
+})(jQuery)
